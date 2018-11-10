6e83a57f0ed7bfac941b10d49563d3e5f3414056
pkohut <pkohut2@gmail.com>
2018-11-10 06:31:04 -0800

    Add ddvplayer dialog and control variables

--- DlgGroupControl.cpp
+++ DlgGroupControl.cpp
@@ -24,6 +24,26 @@ CDlgGroupControl::~CDlgGroupControl()
 void CDlgGroupControl::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST_GC_LAYERS, m_lstLayerNames);
+	DDX_Control(pDX, IDC_LIST_GC_GROUPS, m_lstGroupNames);
+	DDX_Control(pDX, IDC_BUTTON_GC_LAYERS_PICK_ENTITIES, m_btnLayersPick);
+	DDX_Control(pDX, IDC_BUTTON_GC_LAYERS_SELECTALL, m_btnLayersSelectAll);
+	DDX_Control(pDX, IDC_BUTTON_GC_LAYERS_REMOVEALL, m_btnLayersRemoveAll);
+	DDX_Control(pDX, IDC_BUTTON_GC_LAYERS_NEW, m_btnGroupsNew);
+	DDX_Control(pDX, IDC_BUTTON_GC_LAYERS_DELETE, m_btnGroupsDelete);
+	DDX_Control(pDX, IDC_BUTTON_GC_LAYERS_RENAME, m_btnGroupsRename);
+	DDX_Control(pDX, IDC_BUTTON_GC_LAYERS_UPDATE, m_btnGroupsUpdate);
+	DDX_Control(pDX, IDC_BUTTON_GC_LAYERS_LOAD, m_btnGroupsLoad);
+	DDX_Control(pDX, IDC_BUTTON_GC_LAYERS_SAVE, m_btnGroupsSave);
+	DDX_Control(pDX, IDC_BUTTON_GC_LAYERS_NONE, m_btnGroupsNone);
+	DDX_Control(pDX, IDC_BUTTON_GC_LAYERS_APPLY_LAYERTABLE, m_btnApplyLayerTable);
+	DDX_Control(pDX, IDC_BUTTON_GC_LAYERS_APPLY_VIEWPORT, m_btnApplyViewport);
+	DDX_Control(pDX, IDC_BUTTON_GC_LAYERS_ONESTEP, m_btnOneStep);
+	DDX_Control(pDX, IDC_CHECK_GC_LAYERS_OFF, m_chkLayersOff);
+	DDX_Control(pDX, IDC_CHECK_GC_LAYERS_LOCKED, m_chkLayersLocked);
+	DDX_Control(pDX, IDC_CHECK_GC_LAYERS_VP_FROZEN, m_chkLayersVPFrozen);
+	DDX_Control(pDX, IDC_CHECK_GC_LAYERS_FROZEN, m_chkLayersFrozen);
+	DDX_Control(pDX, IDC_CHECK_GC_LAYERS_NVP_FROZEN, m_chkLayersNVPFrozen);
 }
 
 
--- DlgGroupControl.h
+++ DlgGroupControl.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "afxwin.h"
 
 
 // CDlgVplayer dialog
@@ -20,4 +21,25 @@ protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
 	DECLARE_MESSAGE_MAP()
+public:
+	CListBox m_lstLayerNames;
+	CListBox m_lstGroupNames;
+	CButton m_btnLayersPick;
+	CButton m_btnLayersSelectAll;
+	CButton m_btnLayersRemoveAll;
+	CButton m_btnGroupsNew;
+	CButton m_btnGroupsDelete;
+	CButton m_btnGroupsRename;
+	CButton m_btnGroupsUpdate;
+	CButton m_btnGroupsLoad;
+	CButton m_btnGroupsSave;
+	CButton m_btnGroupsNone;
+	CButton m_btnApplyLayerTable;
+	CButton m_btnApplyViewport;
+	CButton m_btnOneStep;
+	CButton m_chkLayersOff;
+	CButton m_chkLayersLocked;
+	CButton m_chkLayersVPFrozen;
+	CButton m_chkLayersFrozen;
+	CButton m_chkLayersNVPFrozen;
 };
